2a59b2fc504d2efe8ce3cd50745a492297fb235b
123Pro123Pro <facebook89911192@gmail.com>
2021-03-31 00:34:41 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from distutils.core import setup
 setup(
   name = 'lamscunpiptest',         # How you named your package folder (MyLib)
   packages = ['lamscunpiptest'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'lamscun',                   # Type in your name
   author_email = 'dreamhopeaaa@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/123Pro123Pro/lamscunpiptest',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/123Pro123Pro/lamscunpiptest/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/123Pro123Pro/lamscunpiptest/archive/refs/tags/v0_2.tar.gz',    # I explain this later on
   keywords = ['lamscun', 'lamscunpiptest', 'lamscunpip'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'json',
-          'requests',
+          
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
